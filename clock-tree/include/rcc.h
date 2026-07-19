b7420404b6edef685ad22e9f231800d0339bdb4e
Jose <githubjose@pm.me>
2026-07-18 22:56:53 -0500

    Configurate clock tree

---
+++ clock-tree/include/rcc.h
@@ -0,0 +1,6 @@
+#ifndef RCC_H
+#define RCC_H
+
+void clock_tree_init(void);
+
+#endif
--- clock-tree/src/rcc.c
+++ clock-tree/src/rcc.c
@@ -1,15 +1,67 @@
+#include "rcc.h"
 #include <stdint.h>
 
 typedef struct {
     volatile uint32_t CR;
+    volatile uint32_t CFGR;
 } RCC_registers;
 
+typedef struct {
+    volatile uint32_t ACR;
+} FLASH_registers;
+
 #define RCC ((RCC_registers *)0x40021000u)
+#define FLASH ((FLASH_registers *)0X40022000u)
 
-void rcc_hse_start(void) {
+void clock_tree_init(void) {
+
+    /* HSE */
     RCC->CR |= (1u << 16);
+
+    while ((RCC->CR & (1u << 17)) == 0u) {
+    }
+
+    /* LATENCY */
+    FLASH->ACR &= ~0x7u;
+    FLASH->ACR |= 0x2u;
+
+    /* HPRE - AHB */
+    RCC->CFGR &= ~(0xFu << 4);
+
+    /* PPRE1 - APB1 */
+    RCC->CFGR &= ~(0x7u << 8);
+    RCC->CFGR |= (0x4u << 8);
+
+    /* PPRE2 - APB2 */
+    RCC->CFGR &= ~(0x7u << 11);
+
+    /* ADCPRE - ADC */
+    RCC->CFGR &= ~(0x3u << 14);
+    RCC->CFGR |= (0x2u << 14);
+
+    /* PLLXTPRE */
+    RCC->CFGR &= ~(1u << 17);
+
+    /* PLLSRC */
+    RCC->CFGR |= (1u << 16);
+
+    /* PLLMUL */
+    RCC->CFGR &= ~(0xFu << 18);
+    RCC->CFGR |= (0x7u << 18);
+
+    /* PLL */
+    RCC->CR |= (1u << 24);
+
+    while ((RCC->CR & (1u << 25)) == 0u) {
+    }
+
+    /* SW */
+    RCC->CFGR &= ~0x3u;
+    RCC->CFGR |= 0x2u;
+
+    while (((RCC->CFGR >> 2) & 0x3u) != 0x2u) {
+    }
+
+    /* Optional - HSION/OFF */
+    RCC->CR &= ~1u;
 }
-
-
-
-

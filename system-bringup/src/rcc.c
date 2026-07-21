57293ac1cdf9e50115bde0e240430b4bc01e298d
Jose <githubjose@pm.me>
2026-07-21 16:09:45 -0500

    refactor: remove local RCC definitions

--- system-bringup/src/rcc.c
+++ system-bringup/src/rcc.c
@@ -1,26 +1,6 @@
 #include <stdint.h>
 #include "rcc.h"
 
-typedef struct {
-    volatile uint32_t CR;
-    volatile uint32_t CFGR;
-    volatile uint32_t CIR;
-    volatile uint32_t APB2RSTR;
-    volatile uint32_t APB1RSTR;
-    volatile uint32_t AHBENR;
-    volatile uint32_t APB2ENR;
-    volatile uint32_t APB1ENR;
-    volatile uint32_t BDCR;
-    volatile uint32_t CSR;
-} RCC_TypeDef;
-
-typedef struct {
-    volatile uint32_t ACR;
-} FLASH_registers;
-
-#define RCC ((RCC_TypeDef *)0x40021000u)
-#define FLASH ((FLASH_registers *)0X40022000u)
-
 void clock_tree_init(void) {
 
     /* HSE */
